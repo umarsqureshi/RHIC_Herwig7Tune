c76dd98901e32fe6dbebfa993a50759cf4129b61
Umar Qureshi <sohailu@gw346.vampire>
2024-03-28 09:30:19 -0500

    Removed extra yoda

--- Herwig_Tune/remove_old_hepmc.sh
+++ Herwig_Tune/remove_old_hepmc.sh
@@ -1,200 +1,200 @@
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0000
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0001
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0002
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0003
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0004
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0005
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0006
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0007
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0008
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0009
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0010
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0011
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0012
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0013
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0014
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0015
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0016
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0017
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0018
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0019
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0020
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0021
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0022
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0023
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0024
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0025
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0026
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0027
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0028
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0029
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0030
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0031
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0032
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0033
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0034
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0035
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0036
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0037
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0038
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0039
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0040
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0041
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0042
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0043
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0044
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0045
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0046
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0047
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0048
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0049
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0050
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0051
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0052
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0053
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0054
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0055
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0056
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0057
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0058
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0059
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0060
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0061
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0062
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0063
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0064
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0065
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0066
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0067
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0068
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0069
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0070
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0071
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0072
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0073
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0074
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0075
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0076
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0077
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0078
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0079
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0080
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0081
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0082
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0083
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0084
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0085
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0086
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0087
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0088
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0089
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0090
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0091
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0092
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0093
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0094
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0095
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0096
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0097
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0098
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
 cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/0099
-rm -r RHIC_Dijet.hepmc
+rm -r 2019.yoda
--- Herwig_Tune/rm_old_hepmc.py
+++ Herwig_Tune/rm_old_hepmc.py
@@ -3,7 +3,7 @@ dirs = [f"{i:04d}" for i in range(100)]
 out = open(f"remove_old_hepmc.sh", 'w')
 
 for dir in dirs:
-    rm = f"cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/{dir}\nrm -r RHIC_Dijet.hepmc\n"
+    rm = f"cd /home/sohailu/RHIC_Herwig7Tune/Herwig_Tune/out/{dir}\nrm -r 2019.yoda\n"
     out.writelines(rm)
     
 out.close()
